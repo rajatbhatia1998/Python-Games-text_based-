2066554e643d4f535f6899007bff8665c5a26db3
Rajat Bhatia <rajat.bhatia859@gmail.com>
2017-12-30 16:43:49 +0530

    Update Rock_Paper_Scissor.py

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,24 +1,28 @@
 
-#game Stone paper sissor
-
+#Rock Paper Scissor Game
+'''How to Play:- 1:Enter your Name 
+2:enter your selection as:-[rock or paper or sic]
+3:Score will be counted untill u lose the game '''
 
 import random
+
 Username=input('Enter your name:')
 Username=Username.upper()
 score=0
 stat='win'
+
 while stat=='win':
     
            print('****************************')   
-           user=input('rock ,paper or sissor:')
-           com=random.choice(['rock','paper','sis'])
+           user=input('rock ,paper or sic:')
+           com=random.choice(['rock','paper','sic'])
            if user == com:
               print('Computer:{} __User:{}\nTRY AGAIN'.format(com,user))
 
               continue
            elif user=='rock':
                
-              
+              #If user input rock then matching with computer selection
                if com=='paper':
                   stat='lose'
                   print('Computer:{} __User:{}'.format(com,user))
@@ -29,7 +33,8 @@ while stat=='win':
                   score+=1
                   print('Computer:{} __User:{}'.format(com,user))
                   print('You '+stat +' ' +Username)
-                
+            
+            # If user input paper then matching with computer selection
            elif user =='paper' :
                if com=='rock':
                   stat='win'
@@ -40,7 +45,9 @@ while stat=='win':
                   stat='lose'
                   print('Computer:{} __User:{}'.format(com,user))
                   print('You '+stat +' ' +Username)
-           elif user=='sis':
+                    
+            #If user input sic then matching with computer selection
+           elif user=='sic':
                if com=='rock':
                   stat='lose'
                   print('Computer:{} __User:{}'.format(com,user))
@@ -51,6 +58,7 @@ while stat=='win':
                   print('Computer:{} __User:{}'.format(com,user))
                   print('You '+stat +' ' +Username)
            else:pass
+        
 print('Game over:')
 print(Username +' Your score is :{}'.format(score))
                 
